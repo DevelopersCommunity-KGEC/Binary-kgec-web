25f76689c5749fc6d09c95d9a2aac72a02f3f4de
imdebamrita <imdebamrita@gmail.com>
2025-03-04 04:05:38 +0530

    platform partner added

--- src/components/Sponsors/index.tsx
+++ src/components/Sponsors/index.tsx
@@ -1,14 +1,19 @@
 import PageSection from '@/hooks/PageSection';
 import BinaryText from '../Animations/BinaryText';
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
 // import useTextScramble from "../Animations/text";
 import { sponsors } from '@/constants/sponsors';
+interface Sponsor {
+  logo: string | StaticImageData;
+  link?: string;
+  alt: string;
+}
 
 const Sponsors = () => {
   return (
     // <PageSection>
-    <div id="sponsors" className="mb-10 flex flex-col font-pixelate text-white sm:mb-10">
+    <div id="sponsors" className="flex flex-col font-pixelate text-white">
       <div>
         <div className="mb-20">
           <BinaryText
@@ -48,22 +53,51 @@ const Sponsors = () => {
 
         {sponsors.map((sponsorItem) => {
           console.log(sponsorItem, 'dfkn');
+
+          let gridClass = "grid w-[calc(80vw)] gap-4 rounded-lg bg-black/5 p-5 md:w-[calc(60vw)]";
+          let imageSizeClass = "w-[200px] h-[200px]"; // Default size
+
+          if (sponsorItem.title === 'Tera Sponsor') {
+            gridClass += " grid-cols-1";
+            imageSizeClass = "w-96 h-32  object-contain";
+          } else if (sponsorItem.title === 'Giga Sponsor') {
+            gridClass += " grid-cols-3";
+            imageSizeClass = "w-[200px] h-[200px] sm:w-[300px] sm:h-[300px] object-contain";
+          } else if (sponsorItem.title === 'Mega Sponsor') {
+            gridClass += " grid-cols-4";
+            imageSizeClass = "w-[150px] h-[150px] sm:w-[250px] sm:h-[250px] object-contain";
+          } else if (sponsorItem.title === 'Kilo Sponsor') {
+            gridClass += " grid-cols-5";
+            imageSizeClass = "w-[100px] h-[100px] sm:w-[200px] sm:h-[200px] object-contain";
+          } else if (sponsorItem.title === 'Platform Partner') {
+            gridClass += " grid-cols-1";
+            imageSizeClass = "w-32 sm:w-28 h-24 object-contain";
+          } else {
+            gridClass += " grid-cols-5";
+          }
+
           return (
             <div className="m-16 flex flex-col items-center justify-center" key={sponsorItem.title}>
               <h2 className="mb-8 text-center text-[1.5em] font-bold md:text-[2rem]">
                 {sponsorItem.title}
               </h2>
-              <div className="flex w-[calc(80vw)] grid-cols-1 items-center justify-center gap-4 rounded-lg bg-black/5 p-5 md:w-[calc(60vw)]">
+              <div className={gridClass}>
                 {sponsorItem.sponsors.length === 0 ? (
                   <div className="text-center" key="default">
                     Coming Soon...
                   </div>
                 ) : (
-                  sponsorItem.sponsors.map((sponsor) => (
-                    <div className="text-center" key={sponsor.link}>
-                      <Link href={sponsor.link}>
-                        <Image src={sponsor.logo} width={550} height={550} alt={sponsor.alt} />
-                      </Link>
+                  sponsorItem.sponsors.map((sponsor: Sponsor) => (
+                    <div className="text-center flex justify-center items-center" key={sponsor.link}>
+                      <a target="_blank" href={sponsor.link}>
+                        <Image
+                          src={sponsor.logo}
+                          width={500}
+                          height={500}
+                          className={imageSizeClass}
+                          alt={sponsor.alt}
+                        />
+                      </a>
                     </div>
                   ))
                 )}
@@ -78,3 +112,68 @@ const Sponsors = () => {
 };
 
 export default Sponsors;
+
+
+
+
+// import BinaryText from '../Animations/BinaryText';
+// import Image, { StaticImageData } from 'next/image';
+// import { sponsors } from '@/constants/sponsors';
+
+// interface Sponsor {
+//   logo: string | StaticImageData;
+//   link?: string;
+//   alt: string;
+// }
+
+// const sizeMap: Record<string, { width: number; height: number }> = {
+//   'Tera Sponsor': { width: 550, height: 550 },
+//   'Giga Sponsor': { width: 500, height: 500 },
+//   'Meta Sponsor': { width: 450, height: 450 },
+//   'Kilo Sponsor': { width: 400, height: 400 },
+//   'Platform Partner': { width: 250, height: 250 },
+//   default: { width: 550, height: 550 },
+// };
+
+// const Sponsors = () => {
+//   return (
+//     <div id="sponsors" className="mb-10 flex flex-col font-pixelate text-white sm:mb-10">
+//       <div className="mb-20">
+//         <BinaryText className="text-[2rem] font-bold text-white md:text-[3rem]" reveal>
+//           <h2 className="relative mb-10 flex items-center font-pixelate font-bold uppercase">
+//             <span className="pl-4 text-green-500 opacity-85">07.</span>
+//             <span className="pl-4 text-gray-200 opacity-85">Sponsors</span>
+//             <div className="ml-4 mt-3 h-[1px] w-[70vh] bg-[#1d6339]"></div>
+//           </h2>
+//         </BinaryText>
+//       </div>
+
+//       {sponsors.map((sponsorItem) => (
+//         <div className="mb-16 flex flex-col items-center" key={sponsorItem.title}>
+//           <h2 className="mb-8 text-center text-[1.5em] font-bold md:text-[2rem]">
+//             {sponsorItem.title}
+//           </h2>
+
+//           <div className="grid w-[90vw] grid-cols-1 gap-6 rounded-lg bg-black/5 p-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+//             {sponsorItem.sponsors.length === 0 ? (
+//               <div className="col-span-full text-center">Coming Soon...</div>
+//             ) : (
+//               sponsorItem.sponsors.map((sponsor: Sponsor) => {
+//                 const { width, height } = sizeMap[sponsorItem.title] || sizeMap.default;
+//                 return (
+//                   <div className="text-center" key={sponsor.link}>
+//                     <a target="_blank" rel="noopener noreferrer" href={sponsor.link}>
+//                       <Image src={sponsor.logo} width={width} height={height} alt={sponsor.alt} />
+//                     </a>
+//                   </div>
+//                 );
+//               })
+//             )}
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Sponsors;
--- src/constants/sponsors.ts
+++ src/constants/sponsors.ts
@@ -1,4 +1,5 @@
 import avalancheSvg from '../../public/sponsors/Avalanche.svg';
+import hackquest from '../../public/sponsors/hackquest.png';
 
 export const sponsors = [
   {
@@ -6,7 +7,7 @@ export const sponsors = [
     sponsors: [
       {
         logo: avalancheSvg,
-        link: '',
+        link: 'https://www.avax.network/',
         alt: 'Avalanve svg',
       },
     ],
@@ -23,4 +24,14 @@ export const sponsors = [
   //   title: 'Kilo Sponsor',
   //   sponsors: [],
   // },
+  {
+    title: 'Platform Partner',
+    sponsors: [
+      {
+        logo: hackquest,
+        link: 'https://www.hackquest.io/',
+        alt: 'hackquest svg',
+      },
+    ],
+  },
 ];
